6fd09bd2553f5740ded38d7df080a30858683a4f
Konstantin Sölch <ks@bergwerk.ag>
2022-03-24 13:46:53 +0100

    init

---
+++ src/pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <Tutorial/>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  head: {
+    title: 'my website title',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'my website description'
+      },
+      {
+        hid: 'og-title',
+        name: 'og:title',
+        content: 'my website og:title'
+      }
+    ],
+  }
+}
+</script>
